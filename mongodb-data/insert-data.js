5082f6595314ea6de025be833bf57657743f07b8
AranBeitia <arantza.beitia@gmail.com>
2021-12-15 16:23:18 +0100

    queries

--- mongodb-data/insert-data.js
+++ mongodb-data/insert-data.js
@@ -1,52 +1,19 @@
 // insert authors
-db.authors.insertMany([
-	{
-		name: 'Penny',
-		lastName: 'Rimbaud',
-		dateBirt: '8-6-1943',
-		dateDeath: '',
-		country: 'Great Britain',
-	},
-	{
-		name: 'Umberto',
-		lastName: 'Eco',
-		datBirt: '5-1-1932',
-		dateDeath: '19-2-2016',
-		country: 'Italy',
-	},
-	{
-		name: 'Hermann',
-		lastName: 'Hesse',
-		dateBirth: '2-7-1877',
-		dateDeath: '9-8-1962',
-		country: 'Germany',
-	},
-])
+db.authors.insert({ name: 'Penny',lastName: 'Rimbaud',dateBirth: '1943-6-8',country: 'Great Britain'})
+db.authors.insert({name: 'Umberto',lastName: 'Eco', dateBirth: '1932-1-5', dateDeath: '2016-2-19', country: 'Italy'})
+db.authors.insert({ name: 'Hermann', lastName: 'Hesse', dateBirth: '1877-2-7', dateDeath: '1962-9-8', country: 'Germany'})
+
 
 // insert books
-db.books.insertMany([
-	{
-		title: 'El lobo estepario',
-		releaseYear: '1927',
-		category: 'bio-fantasy',
-		authors: [
-			{
-				id: '61b9db502140b006d712c6c7',
-				name: 'Herman',
-				lastName: 'Hesse',
-			},
-		],
-	},
-	{
-		title: 'Crass: tienen una bomba',
-		releaseYear: '2008',
-		category: 'arte-punk, politica',
-		authors: [
-			{
-				id: '61b9da362140b006d712c6c5',
-				name: 'Penny',
-				lastName: 'Rimbaud',
-			},
-		],
-	},
-])
+db.books.insert({ title: 'El lobo estepario',releaseYear: '1927',category: 'bio-fantasy',authors: [{id: '61b9db502140b006d712c6c7',name: 'Herman',lastName: 'Hesse',}]})
+db.books.insert({ title: 'Siddhartha',releaseYear: '1922',category: 'novela',authors: [{id: '61b9db502140b006d712c6c7',name: 'Herman',lastName: 'Hesse',}]})
+db.books.insert({ title: 'El último verano de Klingsor',releaseYear: '1983',category: 'novela',authors: [{id: '61b9db502140b006d712c6c7',name: 'Herman',lastName: 'Hesse',}]})
+db.books.insert({ title: 'Crass: tienen una bomba',releaseYear: '2008',category: 'arte-punk, politica',authors: [{id: '61ba02ef2140b006d712c6d3',name: 'Penny',lastName: 'Rimbaud'}]})
+db.books.insert({ title: 'Shibboleth- My Revolting Life',releaseYear: '1999',category: 'philosophy, politics',authors: [{id: '61ba02ef2140b006d712c6d3',name: 'Penny',lastName: 'Rimbaud'}]})
+db.books.insert({ title: 'Nobody\'s Child',releaseYear: '2008',category: 'politics',authors: [{id: '61ba02ef2140b006d712c6d3',name: 'Penny',lastName: 'Rimbaud'}]})
+db.books.insert({ title: 'Particular nonsense',releaseYear: '2010',category: 'politics',authors: [{id: '61ba02ef2140b006d712c6d3',name: 'Penny',lastName: 'Rimbaud'}]})
+db.books.insert({ title: 'El péndulo de Foucault',releaseYear: '1988',category: 'novela',authors: [{id: '61b9db502140b006d712c6c6',name: 'Umberto',lastName: 'Eco'}]})
+db.books.insert({ title: 'Semiótica y filosofía del lenguaje',releaseYear: '1984',category: 'ensayo',authors: [{id: '61b9db502140b006d712c6c6',name: 'Umberto',lastName: 'Eco'}]})
+db.books.insert({ title: 'Entre mentira e ironía',releaseYear: '1998',category: 'ensayo',authors: [{id: '61b9db502140b006d712c6c6',name: 'Umberto',lastName: 'Eco'}]})
+
+
